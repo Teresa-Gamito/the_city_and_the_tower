4b34eedb6fe6f25a728868f620b148b6937fdd51
Hrodrick27 <rodrigo.l.g.valente@gmail.com>
2025-10-26 17:08:09 +0000

    1

--- player.c
+++ player.c
@@ -1,1 +1,58 @@
 #include "player.h"
+
+void playerMoveDirection(char dir) {
+
+    switch(dir) {
+
+        case 'w':
+        case 'W':
+            player.pos_y--;
+            break;
+
+        case 's':
+        case 'S':
+            player.pos_y++;
+            break;
+
+        case 'a':
+        case 'A':
+            player.pos_x--;
+            break;
+
+        case 'd':
+        case 'D':
+            player.pos_x++;
+            break;
+    }
+}
+
+void playerMoveUp() {
+
+    player.pos_y --;
+}
+
+void playerMoveDown() {
+
+    player.pos_y ++;
+}
+
+void playerMoveLeft() {
+
+    player.pos_x --;
+}
+
+void playerMoveRight() {
+
+    player.pos_x ++;
+}
+
+void playerMove(int pos_x, int pos_y) {
+
+    player.pos_x = pos_x;
+    player.pos_y = pos.y;
+}
+
+bool playerHasItem() {
+
+    return player.item 
+}
